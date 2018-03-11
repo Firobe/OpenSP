1dc8b79f0f565aa72c5de343575d1947ffb4f430
Firobe <baboulinet@vps294274.ovh.net>
2018-03-11 01:58:33 +0100

    BOOM

--- src/Player.hpp
+++ src/Player.hpp
@@ -49,15 +49,14 @@ public:
 		revDef1.bodyA = _body;
 		revDef1.bodyB = _rightLeg._body;
 		revDef1.collideConnected = false;
-		revDef1.localAnchorB.Set(+_width/2, 0);
-		revDef1.localAnchorA.Set(_legWidth/2, -_legHeight);
-		revDef1.enableLimit = true;
-		revDef1.lowerAngle = 0;
-		revDef1.upperAngle = rightLooking ? 0 : (90 * DEGTORAD);
-		revDef1.enableMotor = not rightLooking;
+		revDef1.localAnchorA.Set(+_width/2, 0);
+		revDef1.localAnchorB.Set(_legWidth/2, -_legHeight);
+		revDef1.enableLimit = false;
 		revDef1.upperAngle = 0;
+		revDef1.lowerAngle = rightLooking ? (-90 * DEGTORAD) : 0;
+		revDef1.enableMotor = rightLooking;
 		revDef1.maxMotorTorque = 0;
-		revDef1.motorSpeed = 3600;
+		revDef1.motorSpeed = -3600;
 
 		//Attach left
 		b2RevoluteJointDef revDef2;
@@ -108,7 +107,7 @@ public:
 		//_body->ApplyLinearImpulse(
 		//		b2Vec2(unitX, unitY), _body->GetWorldCenter(), true);
 		if(rightLooking)
-			rightJoint->SetMaxMotorTorque(rightJoint->GetMaxMotorTorque() + 0.5);	
+			rightJoint->SetMaxMotorTorque(rightJoint->GetMaxMotorTorque() - 0.5);	
 		else
 			leftJoint->SetMaxMotorTorque(leftJoint->GetMaxMotorTorque() + 0.5);	
 	}

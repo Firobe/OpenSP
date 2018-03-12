e0973880bd7b639cc484e10f5d81bb041521224c
Firobe <baboulinet@vps294274.ovh.net>
2018-03-13 00:48:28 +0100

    la

--- src/Culbuto.hpp
+++ src/Culbuto.hpp
@@ -6,6 +6,27 @@
 #include "Object.hpp"
 #include "Text.hpp"
 
+#define EPSILON (0.1)
+
+class BitoMonitor : public b2ContactListener {
+public:
+	int& nb;
+	BitoMonitor(int& nb) : nb(nb) {}
+private:
+	void BeginContact(b2Contact* contact) {
+		if((long)(contact->GetFixtureA()->GetUserData()) == 0xB170F10 ||
+			(long)(contact->GetFixtureB()->GetUserData()) == 0xB170F10) {
+			++nb;
+		}
+	}
+	void EndContact(b2Contact* contact) {
+		if((long)(contact->GetFixtureA()->GetUserData()) == 0xB170F10 ||
+			(long)(contact->GetFixtureB()->GetUserData()) == 0xB170F10) {
+			--nb;
+		}
+	}
+};
+
 class Culbuto : public Object {
 private:
 	sf::Color _color;
@@ -13,8 +34,13 @@ private:
 	float _height;
 	float _ballRadius = 1 * _width;
 	float _lestDensity = 250.;
+	b2Fixture* bitonio = nullptr;
+	int colNb = 0;
+	BitoMonitor bebouzi;
 public:
-    Culbuto(b2World& world, float x, float y, float w, float h, sf::Color c ) : Object(world), _color(c), _width(w), _height(h) {
+    Culbuto(b2World& world, float x, float y, float w, float h, sf::Color c ) : Object(world), _color(c), _width(w), _height(h), bebouzi(colNb) {
+		world.SetContactListener(&bebouzi);
+
 		// Body definition
         b2BodyDef bDef;
         bDef.type = b2_dynamicBody;
@@ -54,14 +80,31 @@ public:
         fixDef3.shape = &lest;
         _body->CreateFixture(&fixDef3);
 
+		// Bitonio
+		shape.SetAsBox(_width * (1 + EPSILON), EPSILON * _width, b2Vec2(0, -_ballRadius), 0.);
+		fixDef.shape = &shape;
+		fixDef.isSensor = true;
+		bitonio = _body->CreateFixture(&fixDef);
+		bitonio->SetUserData( (void*) 0xB170F10);
+
 		initSprite();
     }
     void initSprite() {
         sf::Vector2f size = sf::Vector2f(_width * RATIO, (_height + _ballRadius) * RATIO);
         _sprite = new sf::RectangleShape(size);
         _sprite->setFillColor(_color);
-        _sprite->setOrigin({_width * RATIO / 2.f, (_height +_ballRadius) * RATIO});
+        _sprite->setOrigin({_width * RATIO / 2.f, _height * RATIO});
     }
+	void update() {
+		if(colliding()) {
+			std::cout << "TOCC" << std::endl;
+		} else {
+			std::cout << "LEMME SMASH" << std::endl;
+		}
+	}
+	bool colliding() const {
+		return colNb > 0;
+	}
 };
 
 #endif

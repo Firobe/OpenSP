a0d4ed6ce34f757b26bcf6bdd863b4f2fc6cd468
Firobe <baboulinet@vps294274.ovh.net>
2018-03-13 18:43:18 +0100

    Autocommit

--- src/Culbuto.cpp
+++ src/Culbuto.cpp
@@ -2,14 +2,14 @@
 
 void BitoMonitor::BeginContact(b2Contact* contact) {
 	void* ua = contact->GetFixtureA()->GetUserData();
-	void* ub = contact->GetFixtureA()->GetUserData();
+	void* ub = contact->GetFixtureB()->GetUserData();
 	if(ua != nullptr) ++((Culbuto*)ua)->colNb;
 	if(ub != nullptr) ++((Culbuto*)ub)->colNb;
 }
 
 void BitoMonitor::EndContact(b2Contact* contact) {
 	void* ua = contact->GetFixtureA()->GetUserData();
-	void* ub = contact->GetFixtureA()->GetUserData();
+	void* ub = contact->GetFixtureB()->GetUserData();
 	if(ua != nullptr) --((Culbuto*)ua)->colNb;
 	if(ub != nullptr) --((Culbuto*)ub)->colNb;
 }
